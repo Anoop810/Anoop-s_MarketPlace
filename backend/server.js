e6b5b74adffbec01f8afb43dcaa34d3662609d99
Anoop810 <anoopsonawane810@gmail.com>
2025-12-01 10:17:22 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  process.env.FRONTEND_URL
+  "https://eloquent-beijinho-b38ff5.netlify.app/"
 ].filter(Boolean)
 
 const corsOptions = {
